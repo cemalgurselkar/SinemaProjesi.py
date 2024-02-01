44eb7336b3ab56b5c974a736e52acae1553e11e6
Cemal Gürsel Kar <116062524+cemalgurselkar@users.noreply.github.com>
2024-02-01 10:24:55 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+import json
+
+class Koltuk:
+    def __init__(self,koltukNo,koltukDurumu,alanKisi,tutar):
+        self.koltukNo = koltukNo
+        self.koltukDurumu = koltukDurumu
+        self.alanKisi = alanKisi
+        self.tutar = tutar
+
+class Film:
+    def __init__(self,isim,resimYolu,id,koltukBilgileri):
+        self.isim = isim
+        self.resimYolu = resimYolu
+        self.id = id
+        self.koltukBilgileri = koltukBilgileri
+
+def veriyiAl():
+    file = open("data.json")
+    data = json.load(file)
+    filmListesi = []
+    for filmIsmi in data.keys():
+        film = data[filmIsmi]
+        koltukBilgileri=[Koltuk(koltuk['koltukNo'],koltuk['koltukDurumu'],koltuk['alanKisi'],koltuk['tutar']) for koltuk in film['koltukBilgileri']]
+        filmListesi.append(Film(filmIsmi,film['image'],film['id'],koltukBilgileri))
+    return filmListesi
+
+filmListesi = veriyiAl()
